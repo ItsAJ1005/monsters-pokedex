6cd0145cc408ff95483d95c0e3658c866e15c5fd
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-08-25 14:45:28 +0530

    Build - migration (Card component from CardList -> CardComponent)

--- src/components/card-list/cardListComponent.jsx
+++ src/components/card-list/cardListComponent.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import './card-list.css'
+import Card from "../card/cardComponent";
 
 
 class CardList extends Component {
@@ -11,17 +12,9 @@ class CardList extends Component {
             <div className="card-list">
                 {monsters.map(monster => {
 
-                    const { name, id } = monster;
-
-                    return (<div className="card-container" key={id}>
-                        <img alt={`monster ${name}`} 
-                        src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                        />
-
-                        <h2>{name}</h2>
-                        <p>{monster.email}</p>
-
-                    </div>)
+                    return (
+                        <Card monster={monster}/>
+                    )
                 })}
             </div>
         )
